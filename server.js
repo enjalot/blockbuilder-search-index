fcf4bcc013f918aad19f7720bef6c1eafdd35cd3
Ian Johnson <enjalot@gmail.com>
2016-02-23 10:37:42 -0800

    get gist from api for indexing on save

--- server.js
+++ server.js
@@ -9,18 +9,22 @@ var config = require('./config.js')
 require('coffee-script/register')
 var content = require('./gist-content')
 var es = require('./elasticsearch')
+var gh = require('./github')
 
 var app = express()
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/index/gist', function(req, res) {
+app.get('/index/gist/:gistId', function(req, res) {
   res.status(200).send("Ok"); // we always send OK, this shouldn't interupt app
-  var gist = req.body;
-  if(!gist) return;
-  content.gistFetcher(gist, function(err) {
-    es.gistParser(gist, function(err) {
-      return;
+  var gistId = req.params.gistId;
+  gh.getGist(gistId, function(err, response, body){
+    if(err) return console.log(gistId, err)
+    var gist = JSON.parse(body)
+    content.gistFetcher(gist, function(err) {
+      es.gistParser(gist, function(err) {
+        return;
+      })
     })
   })
 });
